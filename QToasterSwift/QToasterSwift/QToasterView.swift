0bc2d2a0b3013856fc4e3514d61c9df5925b6068
Ahmad Athaullah <ahmad.athaullah@gmail.com>
2016-07-04 06:01:58 +0700

    use configuration class in QToasterView

--- QToasterSwift/QToasterSwift/QToasterView.swift
+++ QToasterSwift/QToasterSwift/QToasterView.swift
@@ -14,36 +14,24 @@ class QToasterView: UIButton {
     var viewArea = UIView()
     
     var minHeight:CGFloat{
-        return self.statusBarHeight + 40
+        return QToasterConfig.statusBarHeight + 40
     }
     var textSize:CGSize{
         if toaster.iconImage == nil && (toaster.iconURL == nil || toaster.iconURL == "") {
-            return textSize(toaster.text, font: toaster.textFont, maxWidth: self.width - 20)
+            return QToasterConfig.textSize(toaster.text, font: toaster.textFont, maxWidth: QToasterConfig.screenWidth)
         }else{
-            return textSize(toaster.text, font: toaster.textFont, maxWidth: self.width - toaster.iconSquareSize - 25)
+            return QToasterConfig.textSize(toaster.text, font: toaster.textFont, maxWidth: QToasterConfig.screenWidth - toaster.iconSquareSize - 25)
         }
     }
     var titleSize:CGSize{
         if toaster.titleText != nil && toaster.titleText != ""{
             if toaster.iconImage == nil && (toaster.iconURL == nil || toaster.iconURL == "") {
-                return textSize(toaster.titleText!, font: toaster.textFont, maxWidth: self.width - 20)
+                return QToasterConfig.textSize(toaster.titleText!, font: toaster.textFont, maxWidth: QToasterConfig.screenWidth - 20)
             }else{
-                return textSize(toaster.titleText!, font: toaster.textFont, maxWidth: self.width -  toaster.iconSquareSize - 25)
+                return QToasterConfig.textSize(toaster.titleText!, font: toaster.textFont, maxWidth: QToasterConfig.screenWidth -  toaster.iconSquareSize - 25)
             }
         }else{
             return CGSizeMake(0, 0)
-        }
-    }
-    
-    var width:CGFloat{
-        get{
-            return UIScreen.mainScreen().bounds.size.width
-        }
-    }
-    
-    var statusBarHeight:CGFloat{
-        get{
-            return UIApplication.sharedApplication().statusBarFrame.size.height
         }
     }
     
@@ -61,24 +49,25 @@ class QToasterView: UIButton {
     func commonInit(){
         self.backgroundColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0)
         self.tag = 1313
+        self.layer.zPosition = 9999
     }
     
     
     func setupToasterView(toaster: QToasterSwift){
         self.toaster = toaster
         
-        var textAreaWidth =  self.width - 20
+        var textAreaWidth =  QToasterConfig.screenWidth - 20
         var imageToasterHeight:CGFloat = 0
         var textXPos:CGFloat = 10
         
         if toaster.iconImage != nil || (toaster.iconURL != nil && toaster.iconURL != ""){
-            imageToasterHeight = toaster.iconSquareSize + self.statusBarHeight + 20
+            imageToasterHeight = toaster.iconSquareSize + QToasterConfig.statusBarHeight + 20
             textAreaWidth -= (toaster.iconSquareSize + 5)
             toaster.textAlignment = NSTextAlignment.Left
             textXPos += toaster.iconSquareSize + 5
         }
         
-        var toasterHeight = self.textSize.height + self.titleSize.height + self.statusBarHeight + 20
+        var toasterHeight = self.textSize.height + self.titleSize.height + QToasterConfig.statusBarHeight + 20
         if self.titleSize.height > 0 {
             toasterHeight += 3
         }
@@ -91,9 +80,9 @@ class QToasterView: UIButton {
         }
         
         
-        var yPos:CGFloat = self.statusBarHeight + 10
+        var yPos:CGFloat = QToasterConfig.statusBarHeight + 10
         
-        let toasterViewFrame = CGRectMake(0,0 - toasterHeight, self.width,toasterHeight)
+        let toasterViewFrame = CGRectMake(0,0 - toasterHeight, QToasterConfig.screenWidth,toasterHeight)
         
         viewArea.frame = toasterViewFrame
         viewArea.userInteractionEnabled = false
@@ -109,7 +98,7 @@ class QToasterView: UIButton {
                 iconView.image = toaster.iconImage
             }
             if toaster.iconURL != nil && toaster.iconURL != "" {
-                imageForUrl(toaster.iconURL!, completionHandler:{(image: UIImage?, url: String) in
+                QToasterConfig.imageForUrl(toaster.iconURL!, completionHandler:{(image: UIImage?, url: String) in
                     iconView.image = image
                 })
             }
@@ -137,56 +126,33 @@ class QToasterView: UIButton {
         textLabel.font = toaster.textFont
         viewArea.addSubview(textLabel)
         
-        self.frame = CGRectMake(0, 0, self.width, toasterHeight)
+        self.frame = CGRectMake(0, 0, QToasterConfig.screenWidth, toasterHeight)
         self.backgroundColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0)
-        self.addTarget(self, action: #selector(QToasterSwift.touchAction), forControlEvents: UIControlEvents.TouchUpInside)
+        self.addTarget(self, action: #selector(self.toaster.touchAction), forControlEvents: UIControlEvents.TouchUpInside)
         self.addSubview(viewArea)
     }
     
-    func textSize(text: NSString, font: UIFont, maxWidth: CGFloat)->CGSize{
-        let rect = text.boundingRectWithSize(CGSizeMake(maxWidth, CGFloat.max), options: NSStringDrawingOptions.UsesLineFragmentOrigin, attributes: [NSFontAttributeName: font], context: nil) as CGRect
-        return rect.size
-    }
     
-    //  func imageForUrl
-    //  taken from : ImageLoader.swift
-    //  extension
-    //
-    //  Created by Nate Lyman on 7/5/14.
-    //  git: https://github.com/natelyman/SwiftImageLoader
-    //  Copyright (c) 2014 NateLyman.com. All rights reserved.
-    //
-    func imageForUrl(urlString: String, completionHandler:(image: UIImage?, url: String) -> ()) {
-        dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_BACKGROUND, 0), {()in
-            let cache = NSCache()
-            let data: NSData? = cache.objectForKey(urlString) as? NSData
-            
-            if let goodData = data {
-                let image = UIImage(data: goodData)
-                dispatch_async(dispatch_get_main_queue(), {() in
-                    completionHandler(image: image, url: urlString)
-                })
-                return
+    func show(){
+        UIView.animateWithDuration(self.toaster.animateDuration, delay: 0, options: UIViewAnimationOptions.AllowUserInteraction, animations: {
+            let showFrame = CGRectMake(0,0,QToasterConfig.screenWidth,self.frame.height)
+            self.viewArea.frame = showFrame
+            }, completion: { _ in
+                self.hide()
             }
-            
-            let downloadTask: NSURLSessionDataTask = NSURLSession.sharedSession().dataTaskWithURL(NSURL(string: urlString)!, completionHandler: {(data: NSData?, response: NSURLResponse?, error: NSError?) -> Void in
-                if (error != nil) {
-                    completionHandler(image: nil, url: urlString)
-                    return
-                }
-                
-                if let data = data {
-                    let image = UIImage(data: data)
-                    cache.setObject(data, forKey: urlString)
-                    dispatch_async(dispatch_get_main_queue(), {() in
-                        completionHandler(image: image, url: urlString)
-                    })
-                    return
-                }
-                
-            })
-            downloadTask.resume()
-        })
+        )
         
+    }
+    func hide(completion: () -> Void = ({})){
+        UIView.animateWithDuration(self.toaster.animateDuration, delay: self.toaster.delayDuration, options: UIViewAnimationOptions.AllowUserInteraction,
+            animations: {
+                let hideFrame = CGRectMake(0,0 - self.frame.height,QToasterConfig.screenWidth,self.frame.height)
+                self.frame = hideFrame
+            },
+            completion: { _ in
+                self.removeFromSuperview()
+                completion()
+            }
+        )
     }
 }
